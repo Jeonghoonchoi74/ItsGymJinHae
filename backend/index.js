6c44b2df410057b57481e83ce53481b8c3052fd1
Jeonghoonchoi74 <46080119+Jeonghoonchoi74@users.noreply.github.com>
2024-07-16 00:13:56 +0900

    배포 테스트

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ admin.initializeApp({
     type: process.env.FIREBASE_TYPE,
     project_id: process.env.FIREBASE_PROJECT_ID,
     private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),  // 이 줄을 확인하세요
     client_email: process.env.FIREBASE_CLIENT_EMAIL,
     client_id: process.env.FIREBASE_CLIENT_ID,
     auth_uri: process.env.FIREBASE_AUTH_URI,
@@ -212,7 +212,7 @@ app.get('/myReservations', async (req, res) => {
 
 // 회원 탈퇴
 app.post('/deleteAccount', async (req, res) => {
-  const phoneNumber = req.user.phoneNumber;
+  const phoneNumber = req.session.user.phoneNumber;
   try {
     // 회원 정보 삭제
     const userRef = db.collection('users').doc(phoneNumber);
